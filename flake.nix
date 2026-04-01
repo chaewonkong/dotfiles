dde0a3b19b1e056d422ffebd75be67444be2528f
chaewonkong <chaewonkong@gmail.com>
2026-04-02 00:22:27 +0900

    feat: add hyprland

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager config for M2 MacBook Air";
 
   inputs = {
-    nixpkgs.url = "github:nixpkgs/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
--- home.nix
+++ home.nix
@@ -27,8 +27,10 @@
 
   programs.git = {
     enable = true;
-    userName = "leon";
-    userEmail = "chaewonkong@gmail.com";
+    settings = {
+      user.name = "chaewonkong";
+      user.email = "chaewonkong@gmail.com";
+    };
   };
 
   programs.zsh = {
@@ -37,4 +39,97 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
   };
+
+  xdg.configFile."hypr/hyprland.conf".text = ''
+      monitor=,preferred,auto,1
+
+      input {
+        kb_layout = us
+        follow_mouse = 1
+        touchpad {
+          natural_scroll = true
+          tap-to-click = true
+        }
+        sensitivity = 0
+      }
+
+      general {
+        gaps_in = 5
+        gaps_out = 10
+        border_size = 2
+        col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+        col.inactive_border = rgba(595959aa)
+        layout = dwindle
+      }
+
+      decoration {
+        rounding = 10
+        blur {
+          enabled = true
+          size = 3
+          passes = 1
+        }
+        shadow {
+          enabled = true
+          range = 4
+          render_power = 3
+        }
+      }
+
+      animations {
+        enabled = true
+        bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+        animation = windows, 1, 7, myBezier
+        animation = windowsOut, 1, 7, default, popin 80%
+        animation = fade, 1, 7, default
+        animation = workspaces, 1, 6, default
+      }
+
+      dwindle {
+        pseudotile = true
+        preserve_split = true
+      }
+
+      $mod = SUPER
+
+      bind = $mod, Return, exec, kitty
+      bind = $mod, Q, killactive,
+      bind = $mod, M, exit,
+      bind = $mod, V, togglefloating,
+      bind = $mod, D, exec, wofi --show drun
+      bind = $mod, F, fullscreen, 0
+
+      bind = $mod, H, movefocus, l
+      bind = $mod, L, movefocus, r
+      bind = $mod, K, movefocus, u
+      bind = $mod, J, movefocus, d
+
+      bind = $mod, 1, workspace, 1
+      bind = $mod, 2, workspace, 2
+      bind = $mod, 3, workspace, 3
+      bind = $mod, 4, workspace, 4
+      bind = $mod, 5, workspace, 5
+
+      bind = $mod SHIFT, 1, movetoworkspace, 1
+      bind = $mod SHIFT, 2, movetoworkspace, 2
+      bind = $mod SHIFT, 3, movetoworkspace, 3
+      bind = $mod SHIFT, 4, movetoworkspace, 4
+      bind = $mod SHIFT, 5, movetoworkspace, 5
+
+      bindm = $mod, mouse:272, movewindow
+      bindm = $mod, mouse:273, resizewindow
+
+      bind = , Print, exec, grim -g "$(slurp)" - | wl-copy
+
+      binde = , XF86MonBrightnessUp, exec, brightnessctl set 5%+
+      binde = , XF86MonBrightnessDown, exec, brightnessctl set 5%-
+
+      binde = , XF86AudioRaiseVolume, exec, pamixer -i 5
+      binde = , XF86AudioLowerVolume, exec, pamixer -d 5
+      bind = , XF86AudioMute, exec, pamixer -t
+
+      exec-once = waybar
+      exec-once = dunst
+      exec-once = swww-daemon
+    '';
 }
