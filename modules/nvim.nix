a9bc2f8feb9dbf4904da6f7a4c70667b1134d804
chaewonkong <chaewonkong@gmail.com>
2026-09-04 22:16:27 +0900

    chore: unify home-manager path

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,9 +1,10 @@
-{...}:
+{ config, ... }:
+let
+  # 양쪽 머신 모두 repo를 ~/.config/home-manager 에 clone (README/hms alias와 동일한 전제).
+  dotfiles = "${config.home.homeDirectory}/.config/home-manager";
+in
 {
-  # LazyVim 기반 설정. recursive = true로 파일 단위 링크 (디렉토리 통째 링크 X)
-  # 플러그인 버전 변경은 config/nvim/lazy-lock.json을 직접 수정 후 switch
-  xdg.configFile."nvim" = {
-    source = ../config/nvim;
-    recursive = true;
-  };
+  # LazyVim 기반 설정. nix store가 아닌 repo 디렉토리로 직접 링크 → 쓰기 가능.
+  # lazy.nvim이 lazy-lock.json을 갱신하면 그대로 git에 잡힘 (:Lazy update 후 커밋).
+  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/config/nvim";
 }
